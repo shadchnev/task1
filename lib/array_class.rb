33f56186bf0a208678ec6f2d86651ba55fdeb6c6
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2013-12-10 17:57:14 +0000

    feedback

--- lib/array_class.rb
+++ lib/array_class.rb
@@ -1,13 +1,18 @@
 class Array
 
-def chrisinject
-  # shift takes the first element and moves the rest down into the first position
-  # in our case it passes the first element to sum on each iteration of chrisinject
-  sum = self.shift
-  # pass each value from shift into the block, yield takes it from the block and
-  #passes it to the be added
-  self.each { |value| sum = yield(sum, value) }
-  sum
-end
+
+  # First, well done on writing the implementation for inject
+  # It's incomplete but the main idea – calling the block and passing
+  # the result back into the block on each iteration – is there. Well done!
+  def chrisinject
+    # shift takes the first element and moves the rest down into the first position
+    # in our case it passes the first element to sum on each iteration of chrisinject
+    # This is dangerous: you shouldn't be modifying the original array
+    sum = self.shift
+    # pass each value from shift into the block, yield takes it from the block and
+    # passes it to the be added
+    self.each { |value| sum = yield(sum, value) }
+    sum
+  end
 
 end
--- spec/array_class_spec.rb
+++ spec/array_class_spec.rb
@@ -1,30 +1,37 @@
 require_relative '../lib/array_class.rb'
 
 describe Array do
-  
-it "should have my own method for inject" do
-  array = Array.new
+
+# be careful with the indentation: indent everything correctly
+
+# try to choose better names for the tests
+it "should sum an array of numbers" do
+  # you don't have to call Array.new at all
   array = [1, 2, 3, 4]
   expect(array.chrisinject {|sum, value| sum + value}).to eq(10)
 end
 
-it "lets try and different array" do
-  array = Array.new
+it "should sum a different array of numbers" do  
   array = [3, 5, 10, 20]
   expect(array.chrisinject {|sum, value| sum + value}).to eq(38)
 end
 
-it "final time with a muddles up one" do
-  array = Array.new
+it "should sum yet another array of numbers" do  
   array = [13, 1, 6, 45]
   expect(array.chrisinject {|sum, value| sum + value}).to eq(65)
 end
 
-it "may be one more time with some words, nice to see the green in RSPEC :-)" do
-  array = Array.new
+# the names I suggested aren't really good either but you
+# shouldn't have repetitive tests anyway
+
+it "should append strings together" do  
   array = ['my ', 'car ', 'is ', 'a ', 'BMW.']
   expect(array.chrisinject {|sum, value| sum + value}).to eq('my car is a BMW.')
 end
 
+  # you could also have written other tests for other operations, for example:
+  it 'should multiple several numbers' do
+    expect([2,4,5].chrisinject {|product, value| product * value}).to eq(40)
+  end
 
 end
